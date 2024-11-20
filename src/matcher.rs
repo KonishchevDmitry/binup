e5c276b7e71cb8b7d9bcc85d865c02623fdf33a6
Dmitry Konishchev <konishchev@gmail.com>
2024-11-20 20:55:17 +0300

    Make matcher comparable

--- src/matcher.rs
+++ src/matcher.rs
@@ -6,6 +6,8 @@ use regex::Regex;
 use serde::Deserialize;
 use serde::de::{Deserializer, Error};
 
+use crate::core::GenericResult;
+
 #[derive(Clone)]
 pub enum Matcher {
     Glob(GlobMatcher),
@@ -13,6 +15,19 @@ pub enum Matcher {
 }
 
 impl Matcher {
+    pub fn new(pattern: &str) -> GenericResult<Matcher> {
+        Ok(if let Some(regex) = pattern.strip_prefix('~') {
+            Matcher::Regex(Regex::new(regex)?)
+        } else {
+            Matcher::Glob(GlobBuilder::new(&pattern)
+                .literal_separator(true)
+                .backslash_escape(true)
+                .build()?
+                .compile_matcher()
+            )
+        })
+    }
+
     pub fn matches<P: AsRef<Path>>(&self, path: P) -> bool {
         let path = path.as_ref();
 
@@ -23,24 +38,22 @@ impl Matcher {
     }
 }
 
+impl PartialEq for Matcher {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Glob(a), Self::Glob(b)) => a.glob() == b.glob(),
+            (Self::Regex(a), Self::Regex(b)) => a.as_str() == b.as_str(),
+            _ => false,
+        }
+    }
+}
+
 impl<'de> Deserialize<'de> for Matcher {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
         where D: Deserializer<'de>
     {
         let pattern: String = Deserialize::deserialize(deserializer)?;
-
-        Ok(if let Some(regex) = pattern.strip_prefix('~') {
-            Matcher::Regex(
-                Regex::new(regex).map_err(|e| D::Error::custom(e.to_string()))?
-            )
-        } else {
-            Matcher::Glob(GlobBuilder::new(&pattern)
-                .literal_separator(true)
-                .backslash_escape(true)
-                .build().map_err(|e| D::Error::custom(e.to_string()))?
-                .compile_matcher()
-            )
-        })
+        Matcher::new(&pattern).map_err(|e| D::Error::custom(e.to_string()))
     }
 }
 
