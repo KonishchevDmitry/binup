561a1b709541e0fdb4a7ada506d1991475960728
Dmitry Konishchev <konishchev@gmail.com>
2026-01-11 19:09:28 +0300

    Support non-archive (single binary) releases

--- src/download.rs
+++ src/download.rs
@@ -6,11 +6,10 @@ use reqwest::blocking::ClientBuilder;
 use tar::{Archive, EntryType};
 use url::Url;
 
-use crate::core::EmptyResult;
+use crate::core::{EmptyResult, GenericResult};
 use crate::util;
 
 pub enum FileType {
-    #[allow(dead_code)] // FIXME(konishchev): Implement it
     Single,
     Archived {mode: u32},
 }
@@ -21,19 +20,25 @@ pub trait Installer {
 }
 
 pub fn download(url: &Url, name: &str, installer: &mut dyn Installer) -> EmptyResult {
-    let mut stripped_name = name;
+    let (stripped_name, decoder_builder) = get_decoder_builder(name)?;
+    let archive_type = stripped_name.rsplit_once('.').map(|(_, extension)| extension);
 
-    let mut decoder_builder: DecoderBuilder = Box::new(|reader| Box::new(reader));
-    if let Some((prefix, extension)) = stripped_name.rsplit_once('.')
-        && let Some(builder) = get_decoder_builder(extension) {
-        decoder_builder = builder;
-        stripped_name = prefix;
-    }
+    let reader_builder: ReaderBuilder = match archive_type.unwrap_or_default() {
+        "tar" => Box::new(|| Box::new(TarReader::new())),
+        "7z" | "apk" | "deb" | "dmg" | "msi" | "pkg" | "rar" | "rpm" | "zip" => {
+            return Err!("Unsupported file type: {name:?}")
+        },
+        _ => {
+            if installer.has_binary_matcher() {
+                return Err!(concat!(
+                    "The release file {:?} looks like a simple binary (not an archive), ",
+                    "but a binary matcher is specified",
+                ), name);
+            }
 
-    let reader_builder: ReaderBuilder = match stripped_name.rsplit_once('.') {
-        Some((_, "tar")) => Box::new(|| Box::new(TarReader::new())),
-        // XXX(konishchev): + installer.has_binary_matcher()
-        _ => return Err!("Unsupported file type: {name:?}")
+            let name = name.to_owned();
+            Box::new(move || Box::new(BinaryReader::new(&name)))
+        },
     };
 
     debug!("Downloading {url}...");
@@ -52,19 +57,44 @@ pub fn download(url: &Url, name: &str, installer: &mut dyn Installer) -> EmptyRe
 
 type DecoderBuilder = Box<dyn FnOnce(Box<dyn Read>) -> Box<dyn Read>>;
 
-fn get_decoder_builder(extension: &str) -> Option<DecoderBuilder> {
-    Some(match extension {
+fn get_decoder_builder(name: &str) -> GenericResult<(&str, DecoderBuilder)> {
+    let Some((stripped_name, extension)) = name.rsplit_once('.') else {
+        return Ok((name, Box::new(|reader| reader)));
+    };
+
+    let builder: DecoderBuilder = match extension {
         "bz2" => Box::new(|reader| Box::new(bzip2::read::BzDecoder::new(reader))),
         "gz" => Box::new(|reader| Box::new(flate2::read::GzDecoder::new(reader))),
         "xz" => Box::new(|reader| Box::new(xz2::read::XzDecoder::new(reader))),
-        _ => return None,
-    })
+        "lz" | "lz4" | "lzma" | "lzo" | "z" | "zst" => return Err!("Unsupported file type: {name:?}"),
+        _ => return Ok((name, Box::new(|reader| reader))),
+    };
+
+    Ok((stripped_name, builder))
 }
 
 type ReaderBuilder = Box<dyn FnOnce() -> Box<dyn ReleaseReader>>;
 
 trait ReleaseReader {
     fn read(self: Box<Self>, reader: Box<dyn Read>, installer: &mut dyn Installer) -> EmptyResult;
+}
+
+struct BinaryReader {
+    name: String,
+}
+
+impl BinaryReader {
+    fn new(name: &str) -> BinaryReader {
+        BinaryReader {
+            name: name.to_owned(),
+        }
+    }
+}
+
+impl ReleaseReader for BinaryReader {
+    fn read(self: Box<Self>, mut reader: Box<dyn Read>, installer: &mut dyn Installer) -> EmptyResult {
+        installer.on_file(Path::new(self.name.as_str()), FileType::Single, &mut reader)
+    }
 }
 
 struct TarReader {
