9fe6a998daefac800de5325283575c7e27fb139d
Dmitry Konishchev <konishchev@gmail.com>
2026-01-11 15:26:44 +0300

    Generalize release downloading logic

--- src/download.rs
+++ src/download.rs
@@ -6,7 +6,7 @@ use reqwest::blocking::ClientBuilder;
 use tar::{Archive, EntryType};
 use url::Url;
 
-use crate::core::{EmptyResult, GenericResult};
+use crate::core::EmptyResult;
 use crate::util;
 
 pub trait Installer {
@@ -14,68 +14,83 @@ pub trait Installer {
 }
 
 pub fn download(url: &Url, name: &str, installer: &mut dyn Installer) -> EmptyResult {
-    let reader = ReleaseReaderBuilder::new(name)?;
-    let client = ClientBuilder::new().user_agent(util::USER_AGENT).build()?;
+    let mut stripped_name = name;
+
+    let mut decoder_builder: DecoderBuilder = Box::new(|reader| Box::new(reader));
+    if let Some((prefix, extension)) = stripped_name.rsplit_once('.')
+        && let Some(builder) = get_decoder_builder(extension) {
+        decoder_builder = builder;
+        stripped_name = prefix;
+    }
+
+    let reader_builder: ReaderBuilder = match stripped_name.rsplit_once('.') {
+        Some((_, "tar")) => Box::new(|| Box::new(TarReader::new())),
+        _ => return Err!("Unsupported file type: {name:?}")
+    };
 
     debug!("Downloading {url}...");
+    let client = ClientBuilder::new().user_agent(util::USER_AGENT).build()?;
 
     let response = client.get(url.to_owned()).send()?;
     if !response.status().is_success() {
-        return Err!("The server returned and error: {}", response.status())
+        return Err!("The server returned an error: {}", response.status())
     }
 
-    let mut archive = reader.build(response);
+    let decoder = decoder_builder(Box::new(response));
+    let reader = reader_builder();
 
-    for (index, entry) in archive.entries()?.enumerate() {
-        let mut entry = entry?;
-
-        let header = entry.header();
-        let path = entry.path()?;
-        let entry_type = header.entry_type();
-
-        if index == 0 {
-            debug!("Processing the archive:")
-        }
-        debug!("* {path:?} ({entry_type:?})");
-
-        if matches!(entry_type, EntryType::Regular | EntryType::Continuous) {
-            let path = path.to_path_buf();
-            let mode = header.mode()?;
-            installer.on_file(&path, mode, &mut entry)?;
-        }
-    }
-
-    Ok(())
+    reader.read(decoder, installer)
 }
 
 type DecoderBuilder = Box<dyn FnOnce(Box<dyn Read>) -> Box<dyn Read>>;
 
-struct ReleaseReaderBuilder {
-    decoder_builder: DecoderBuilder,
+fn get_decoder_builder(extension: &str) -> Option<DecoderBuilder> {
+    Some(match extension {
+        "bz2" => Box::new(|reader| Box::new(bzip2::read::BzDecoder::new(reader))),
+        "gz" => Box::new(|reader| Box::new(flate2::read::GzDecoder::new(reader))),
+        "xz" => Box::new(|reader| Box::new(xz2::read::XzDecoder::new(reader))),
+        _ => return None,
+    })
 }
 
-impl ReleaseReaderBuilder {
-    fn new(name: &str) -> GenericResult<ReleaseReaderBuilder> {
-        let decoder_builder = name.rsplit_once('.').and_then(|(name, extension)| {
-            let decoder: DecoderBuilder = match extension {
-                "bz2" => Box::new(|reader| Box::new(bzip2::read::BzDecoder::new(reader))),
-                "gz" => Box::new(|reader| Box::new(flate2::read::GzDecoder::new(reader))),
-                "xz" => Box::new(|reader| Box::new(xz2::read::XzDecoder::new(reader))),
-                _ => return None,
-            };
+type ReaderBuilder = Box<dyn FnOnce() -> Box<dyn ReleaseReader>>;
 
-            if name.rsplit_once('.')?.1 != "tar" {
-                return None;
-            }
+trait ReleaseReader {
+    fn read(self: Box<Self>, reader: Box<dyn Read>, installer: &mut dyn Installer) -> EmptyResult;
+}
 
-            Some(decoder)
-        }).ok_or_else(|| format!("Unsupported file type: {name:?}"))?;
+struct TarReader {
+}
 
-        Ok(ReleaseReaderBuilder {decoder_builder})
+impl TarReader {
+    fn new() -> TarReader {
+        TarReader {}
     }
+}
 
-    fn build<R: Read + 'static>(self, reader: R) -> Archive<impl Read> {
-        let reader = (self.decoder_builder)(Box::new(reader));
-        Archive::new(reader)
+impl ReleaseReader for TarReader {
+    fn read(self: Box<Self>, reader: Box<dyn Read>, installer: &mut dyn Installer) -> EmptyResult {
+        let mut archive = Archive::new(reader);
+
+        for (index, entry) in archive.entries()?.enumerate() {
+            let mut entry = entry?;
+
+            let header = entry.header();
+            let path = entry.path()?;
+            let entry_type = header.entry_type();
+
+            if index == 0 {
+                debug!("Processing the archive:")
+            }
+            debug!("* {path:?} ({entry_type:?})");
+
+            if matches!(entry_type, EntryType::Regular | EntryType::Continuous) {
+                let path = path.to_path_buf();
+                let mode = header.mode()?;
+                installer.on_file(&path, mode, &mut entry)?;
+            }
+        }
+
+        Ok(())
     }
 }
