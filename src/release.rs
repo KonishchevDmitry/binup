7244afe499ffa04fe85c5379c70448c8aef8a07c
Dmitry Konishchev <konishchev@gmail.com>
2026-06-21 20:39:40 +0300

    Automatically select releases with GNU libc ABI for Linux

--- src/release.rs
+++ src/release.rs
@@ -1,6 +1,6 @@
 use std::borrow::Borrow;
 use std::env::consts;
-#[cfg(test)] use std::ops::Deref;
+use std::fmt::Write;
 use std::str::FromStr;
 
 use chrono::{DateTime, Utc};
@@ -58,28 +58,15 @@ impl Release {
             });
         }
 
-        let matchers = generate_release_matchers(binary_name, &self.project.name, consts::OS, consts::ARCH)
-            .unwrap_or_default();
-
-        for matcher in matchers {
-            let assets: Vec<_> = self.assets.iter()
-                .filter(|asset| matcher.matches(&asset.name))
-                .collect();
-
-            if assets.is_empty() {
-                debug!("No assets match `{matcher}` automatic release matcher.");
-            } else {
-                debug!("Matched assets for `{matcher}` automatic release matcher:{}", format_assets(&assets));
-            }
-
-            if assets.len() == 1 {
-                return Ok(assets[0]);
+        if let Some(matchers) = generate_release_matchers(binary_name, &self.project.name, consts::OS, consts::ARCH) {
+            if let Some((_matcher, asset)) = match_assets(&self.assets, &matchers, consts::OS) {
+                return Ok(asset);
             }
         }
 
         Err!(concat!(
             "Unable to automatically choose the proper release from the following assets:{}\n\n",
-            "Release matcher should be specifed.",
+            "Release matcher should be specified.",
         ), format_assets(&self.assets))
     }
 }
@@ -96,13 +83,16 @@ fn format_assets<A: Borrow<Asset>>(assets: &[A]) -> String {
     }))
 }
 
+const SEPARATOR_REGEX: &str = "[-._]";
+
 fn generate_release_matchers(binary_name: &str, project_name: &str, os: &str, arch: &str) -> Option<Vec<Matcher>> {
     let os = OS::from_str(os).ok()?;
     let arch = Arch::from_str(arch).ok()?;
 
+    // vendor + os
     let os_regex = match os {
-        OS::Linux => "linux",
-        OS::MacOS => "(?:apple-darwin|darwin|macos)",
+        OS::Linux => "(?:unknown-)?linux",
+        OS::MacOS => "(?:apple-)?(?:darwin|macos)",
         _ => return None,
     };
 
@@ -112,12 +102,10 @@ fn generate_release_matchers(binary_name: &str, project_name: &str, os: &str, ar
         _ => return None,
     };
 
-    let separator_regex = "[-._]";
-    let any_fields_regex = format!("(?:{separator_regex}[^/]+)?");
-
+    let any_fields_regex = format!("(?:{SEPARATOR_REGEX}[^/]+)?");
     let platform_regex = format!("(?:{os_regex}[-_]{arch_regex}|{arch_regex}[-_]{os_regex})");
     let optional_compression_extension_regex = format!(r"(?:{COMPRESSION_EXTENSION_REGEX})?");
-    let archive_regex = format!(r"{separator_regex}{platform_regex}{any_fields_regex}{ARCHIVE_EXTENSION_REGEX}$");
+    let archive_regex = format!(r"{SEPARATOR_REGEX}{platform_regex}{any_fields_regex}{ARCHIVE_EXTENSION_REGEX}$");
 
     // Prioritized list of matchers
     let mut matchers = Vec::new();
@@ -130,7 +118,7 @@ fn generate_release_matchers(binary_name: &str, project_name: &str, os: &str, ar
 
         // Binary with strict name and platform spec
         matchers.push(Regex::new(&format!(
-            r"^{name_regex}{separator_regex}{platform_regex}{optional_compression_extension_regex}$")).unwrap());
+            r"^{name_regex}{SEPARATOR_REGEX}{platform_regex}{optional_compression_extension_regex}$")).unwrap());
     }
 
     // Archive with strict platform spec and relaxed name spec
@@ -139,7 +127,7 @@ fn generate_release_matchers(binary_name: &str, project_name: &str, os: &str, ar
     for name_regex in &name_regexes {
         // Binary with strict name spec and relaxed platform spec (example: a Linux-only tool)
         matchers.push(Regex::new(&format!(
-            r"^{name_regex}{separator_regex}{arch_regex}{optional_compression_extension_regex}$")).unwrap());
+            r"^{name_regex}{SEPARATOR_REGEX}{arch_regex}{optional_compression_extension_regex}$")).unwrap());
 
         // Binary with strict name spec and with no platform spec (example: a Linux-only tool for a single architecture)
         matchers.push(Regex::new(&format!(
@@ -147,6 +135,97 @@ fn generate_release_matchers(binary_name: &str, project_name: &str, os: &str, ar
     }
 
     Some(matchers.into_iter().map(Matcher::Regex).collect())
+}
+
+fn match_assets<'a>(assets: &'a [Asset], matchers: &[Matcher], os: &str) -> Option<(usize, &'a Asset)> {
+    for (index, matcher) in matchers.iter().enumerate() {
+        let mut assets: Vec<_> = assets.into_iter()
+            .filter(|asset| matcher.matches(&asset.name))
+            .collect();
+
+        // Linux tools are often compiled for multiple ABI and we want to have a sane default to automatically choose
+        // between them. GNU libc looks as a good default here (https://konishchev.ru/posts/glibc-static-linking/).
+        //
+        // We do it as a separate hack to not overcomplicate matching logic.
+        if assets.len() > 1 && OS::from_str(os) == Ok(OS::Linux) {
+            if let Some(asset) = fold_assets_by_abi(&assets, &["linux-gnu", "linux-musl"]) {
+                assets.truncate(0);
+                assets.push(asset);
+            }
+        }
+
+        if assets.is_empty() {
+            debug!("No assets match `{matcher}` automatic release matcher.");
+        } else {
+            debug!("Matched assets for `{matcher}` automatic release matcher:{}", format_assets(&assets));
+        }
+
+        if assets.len() == 1 {
+            return Some((index, assets[0]));
+        }
+    }
+
+    None
+}
+
+// An asset is selected only when ABI is the only difference with other assets
+fn fold_assets_by_abi<'a>(assets: &[&'a Asset], prioritized_abi: &[&str]) -> Option<&'a Asset> {
+    let abi_regex = {
+        let mut abi_regex = format!(r"{SEPARATOR_REGEX}(?<abi>");
+
+        for (index, abi) in prioritized_abi.into_iter().enumerate() {
+            if index != 0 {
+                abi_regex.push('|');
+            }
+            abi_regex.push_str(&regex::escape(abi));
+        }
+
+        write!(&mut abi_regex, ")(?:{SEPARATOR_REGEX}|$)").unwrap();
+        Regex::new(&abi_regex).unwrap()
+    };
+
+    struct SelectedAsset<'a> {
+        abi: &'a str,
+        asset: &'a Asset,
+        stripped_name: String,
+    }
+
+    let mut selected: Option<SelectedAsset> = None;
+
+    for &asset in assets {
+        let name = &asset.name;
+        let captures = abi_regex.captures(name)?;
+
+        let abi_capture = captures.name("abi").unwrap();
+        if abi_regex.find_at(name, abi_capture.end()).is_some() {
+            return None;
+        }
+
+        let current_abi = abi_capture.as_str();
+        let stripped_name = format!("{}{}",
+            &name[..captures.get_match().start()],
+            &name[abi_capture.end()..]);
+
+        if let Some(selected) = selected.as_ref() {
+            if current_abi == selected.abi || stripped_name != selected.stripped_name {
+                return None;
+            }
+
+            let current_priority = prioritized_abi.iter().position(|&abi| abi == current_abi).unwrap();
+            let selected_priority = prioritized_abi.iter().position(|&abi| abi == selected.abi).unwrap();
+            if current_priority >= selected_priority {
+                continue;
+            }
+        }
+
+        selected = Some(SelectedAsset {
+            abi: current_abi,
+            asset,
+            stripped_name,
+        });
+    }
+
+    selected.map(|selected| selected.asset)
 }
 
 pub fn generate_binary_matcher(binary_name: &str, release: &Release) -> Matcher {
@@ -196,13 +275,15 @@ mod tests {
     #[rstest(binary_name, project_name, assets, matches, matcher_index,
         // https://github.com/KonishchevDmitry/binup
         case("binup", "binup", &[
+            "binup-v2.0.0-linux-arm64.tar.bz2",
             "binup-v1.1.0-linux-x64.tar.bz2",
             "binup-v1.1.0-macos-arm64.tar.bz2",
             "binup-v1.1.0-macos-x64.tar.bz2",
         ], &[
-            (OS::Linux, Arch::X86_64, "binup-v1.1.0-linux-x64.tar.bz2"),
-            (OS::MacOS, Arch::X86_64, "binup-v1.1.0-macos-x64.tar.bz2"),
-            (OS::MacOS, Arch::AArch64, "binup-v1.1.0-macos-arm64.tar.bz2"),
+            (OS::Linux, Arch::X86_64, Some("binup-v1.1.0-linux-x64.tar.bz2")),
+            (OS::Linux, Arch::AArch64, Some("binup-v2.0.0-linux-arm64.tar.bz2")),
+            (OS::MacOS, Arch::X86_64, Some("binup-v1.1.0-macos-x64.tar.bz2")),
+            (OS::MacOS, Arch::AArch64, Some("binup-v1.1.0-macos-arm64.tar.bz2")),
         ], 0),
 
         // https://github.com/DNSCrypt/dnscrypt-proxy
@@ -260,9 +341,10 @@ mod tests {
             "dnscrypt-proxy-win64-2.1.5.zip",
             "dnscrypt-proxy-win64-2.1.5.zip.minisig",
         ], &[
-            (OS::Linux, Arch::X86_64, "dnscrypt-proxy-linux_x86_64-2.1.5.tar.gz"),
-            (OS::MacOS, Arch::X86_64, "dnscrypt-proxy-macos_x86_64-2.1.5.zip"),
-            (OS::MacOS, Arch::AArch64, "dnscrypt-proxy-macos_arm64-2.1.5.zip"),
+            (OS::Linux, Arch::X86_64, Some("dnscrypt-proxy-linux_x86_64-2.1.5.tar.gz")),
+            (OS::Linux, Arch::AArch64, Some("dnscrypt-proxy-linux_arm64-2.1.5.tar.gz")),
+            (OS::MacOS, Arch::X86_64, Some("dnscrypt-proxy-macos_x86_64-2.1.5.zip")),
+            (OS::MacOS, Arch::AArch64, Some("dnscrypt-proxy-macos_arm64-2.1.5.zip")),
         ], 0),
 
         // https://github.com/martin-helmich/prometheus-nginxlog-exporter
@@ -277,9 +359,10 @@ mod tests {
             "prometheus-nginxlog-exporter_1.11.0_linux_arm64.rpm",
             "prometheus-nginxlog-exporter_1.11.0_linux_arm64.tar.gz",
         ], &[
-            (OS::Linux, Arch::X86_64, "prometheus-nginxlog-exporter_1.11.0_linux_amd64.tar.gz"),
-            (OS::MacOS, Arch::X86_64, "prometheus-nginxlog-exporter_1.11.0_darwin_amd64.tar.gz"),
-            (OS::MacOS, Arch::AArch64, "prometheus-nginxlog-exporter_1.11.0_darwin_arm64.tar.gz"),
+            (OS::Linux, Arch::X86_64, Some("prometheus-nginxlog-exporter_1.11.0_linux_amd64.tar.gz")),
+            (OS::Linux, Arch::AArch64, Some("prometheus-nginxlog-exporter_1.11.0_linux_arm64.tar.gz")),
+            (OS::MacOS, Arch::X86_64, Some("prometheus-nginxlog-exporter_1.11.0_darwin_amd64.tar.gz")),
+            (OS::MacOS, Arch::AArch64, Some("prometheus-nginxlog-exporter_1.11.0_darwin_arm64.tar.gz")),
         ], 0),
 
         // https://github.com/prometheus/node_exporter
@@ -305,9 +388,10 @@ mod tests {
             "node_exporter-1.8.2.openbsd-amd64.tar.gz",
             "sha256sums.txt",
         ], &[
-            (OS::Linux, Arch::X86_64, "node_exporter-1.8.2.linux-amd64.tar.gz"),
-            (OS::MacOS, Arch::X86_64, "node_exporter-1.8.2.darwin-amd64.tar.gz"),
-            (OS::MacOS, Arch::AArch64, "node_exporter-1.8.2.darwin-arm64.tar.gz"),
+            (OS::Linux, Arch::X86_64, Some("node_exporter-1.8.2.linux-amd64.tar.gz")),
+            (OS::Linux, Arch::AArch64, Some("node_exporter-1.8.2.linux-arm64.tar.gz")),
+            (OS::MacOS, Arch::X86_64, Some("node_exporter-1.8.2.darwin-amd64.tar.gz")),
+            (OS::MacOS, Arch::AArch64, Some("node_exporter-1.8.2.darwin-arm64.tar.gz")),
         ], 2),
 
         // https://github.com/shadowsocks/shadowsocks-rust
@@ -343,10 +427,48 @@ mod tests {
             "shadowsocks-v1.20.3.x86_64-unknown-linux-musl.tar.xz",
             "shadowsocks-v1.20.3.x86_64-unknown-linux-musl.tar.xz.sha256",
         ], &[
-            // TODO(konishchev): Always automatically select glibc variant?
-            // (OS::Linux, Arch::X86_64, "shadowsocks-v1.20.3.x86_64-unknown-linux-gnu.tar.xz"),
-            (OS::MacOS, Arch::X86_64, "shadowsocks-v1.20.3.x86_64-apple-darwin.tar.xz"),
-            (OS::MacOS, Arch::AArch64, "shadowsocks-v1.20.3.aarch64-apple-darwin.tar.xz"),
+            (OS::Linux, Arch::X86_64, Some("shadowsocks-v1.20.3.x86_64-unknown-linux-gnu.tar.xz")),
+            (OS::Linux, Arch::AArch64, Some("shadowsocks-v1.20.3.aarch64-unknown-linux-gnu.tar.xz")),
+            (OS::MacOS, Arch::X86_64, Some("shadowsocks-v1.20.3.x86_64-apple-darwin.tar.xz")),
+            (OS::MacOS, Arch::AArch64, Some("shadowsocks-v1.20.3.aarch64-apple-darwin.tar.xz")),
+        ], 4),
+
+        // https://github.com/telemt/telemt
+        case("telemt", "telemt", &[
+            "telemt",
+            "telemt-aarch64-linux-gnu.tar.gz",
+            "telemt-aarch64-linux-gnu.tar.gz.sha256",
+            "telemt-aarch64-linux-musl.tar.gz",
+            "telemt-aarch64-linux-musl.tar.gz.sha256",
+            "telemt-x86_64-linux-gnu.tar.gz",
+            "telemt-x86_64-linux-gnu.tar.gz.sha256",
+            "telemt-x86_64-linux-musl.tar.gz",
+            "telemt-x86_64-linux-musl.tar.gz.sha256",
+            "telemt-x86_64-v3-linux-gnu.tar.gz",
+            "telemt-x86_64-v3-linux-gnu.tar.gz.sha256",
+            "telemt-x86_64-v3-linux-musl.tar.gz",
+            "telemt-x86_64-v3-linux-musl.tar.gz.sha256",
+        ], &[
+            (OS::Linux, Arch::X86_64, Some("telemt-x86_64-linux-gnu.tar.gz")),
+            (OS::Linux, Arch::AArch64, Some("telemt-aarch64-linux-gnu.tar.gz")),
+        ], 0),
+        case("telemt", "telemt", &[
+            "telemt",
+            "telemt-aarch64-linux-gnu.tar.gz",
+            "telemt-aarch64-linux-gnu.tar.gz.sha256",
+            "telemt-aarch64-linux-musl.tar.gz",
+            "telemt-aarch64-linux-musl.tar.gz.sha256",
+            "telemt-x86_64-linux-gnu.tar.gz",
+            "telemt-x86_64-linux-gnu.tar.gz.sha256",
+            "telemt-x86_64-linux-musl.tar.gz",
+            "telemt-x86_64-linux-musl.tar.gz.sha256",
+            "telemt-x86_64-v3-linux-gnu.tar.gz",
+            "telemt-x86_64-v3-linux-gnu.tar.gz.sha256",
+            "telemt-x86_64-v3-linux-musl.tar.gz",
+            "telemt-x86_64-v3-linux-musl.tar.gz.sha256",
+        ], &[
+            // We don't want this decision actually, but it's an artifact of current release matcher
+            (OS::MacOS, Arch::AArch64, Some("telemt")),
         ], 4),
 
         // https://github.com/tsl0922/ttyd/releases
@@ -364,40 +486,37 @@ mod tests {
             "ttyd.win32.exe",
             "ttyd.x86_64",
         ], &[
-            (OS::Linux, Arch::X86_64, "ttyd.x86_64"),
-            (OS::MacOS, Arch::X86_64, "ttyd.x86_64"),
-            (OS::MacOS, Arch::AArch64, "ttyd.aarch64"),
+            // There is no OS in the assets, so we match it to any OS, assuming that it's available for user's platform only
+            (OS::Linux, Arch::X86_64, Some("ttyd.x86_64")),
+            (OS::Linux, Arch::AArch64, Some("ttyd.aarch64")),
+            (OS::MacOS, Arch::X86_64, Some("ttyd.x86_64")),
+            (OS::MacOS, Arch::AArch64, Some("ttyd.aarch64")),
         ], 3),
 
         // https://github.com/KonishchevDmitry/binup/issues/2#issuecomment-3222682495
         case("rapidgzip", "indexed_bzip2", &["rapidgzip"], &[
-            (OS::Linux, Arch::X86_64, "rapidgzip"),
-            (OS::MacOS, Arch::X86_64, "rapidgzip"),
-            (OS::MacOS, Arch::AArch64, "rapidgzip"),
+            (OS::Linux, Arch::X86_64, Some("rapidgzip")),
+            (OS::MacOS, Arch::X86_64, Some("rapidgzip")),
+            (OS::MacOS, Arch::AArch64, Some("rapidgzip")),
         ], 6),
     )]
-    fn release_matcher(binary_name: &str, project_name: &str, assets: &[&str], matches: &[(OS, Arch, &str)], matcher_index: usize) {
+    fn release_matcher(binary_name: &str, project_name: &str, assets: &[&str], matches: &[(OS, Arch, Option<&str>)], matcher_index: usize) {
         for (os, arch, expected) in matches {
+            let assets = assets.iter().map(|&name| Asset {
+                name: name.to_owned(),
+                time: Utc::now(),
+                url: Url::parse("https://github.com/").unwrap(),
+            }).collect_vec();
+
             let matchers = generate_release_matchers(binary_name, project_name, os.as_str(), arch.as_str()).unwrap();
 
-            for (index, matcher) in matchers[..matcher_index].iter().enumerate() {
-                println!("#{index}: {matcher}");
-
-                let result: Vec<&str> = assets.iter()
-                    .filter(|asset| matcher.matches(asset))
-                    .map(Deref::deref).collect();
-
-                assert_eq!(result, Vec::<&str>::new(), "{os}/{arch}");
+            match match_assets(&assets, &matchers, os.as_str()) {
+                Some((index, asset)) => assert_eq!(
+                    (index, Some(asset.name.as_str())),
+                    (matcher_index, *expected),
+                ),
+                None => assert_eq!(*expected, None),
             }
-
-            let matcher = &matchers[matcher_index];
-            println!("#{matcher_index}: {matcher}");
-
-            let result: Vec<&str> = assets.iter()
-                .filter(|asset| matcher.matches(asset))
-                .map(Deref::deref).collect();
-
-            assert_eq!(&result, &[expected as &str]);
         }
     }
 
